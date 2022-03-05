28430abdcf1f7409e98a0e7fa4e505789c8bb9ee
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-03-05 23:27:01 +0100

    refactor: rename test_suite to funcmap_tests

---
+++ funcmap_tests/src/lib.rs
@@ -0,0 +1,1 @@
+// nothing here, just to avoid a warning because funcmap_tests-tests has an implicit dependency on the lib crate of funcmap_tests
--- test_suite/src/lib.rs
+++
@@ -1,1 +0,0 @@
-// nothing here, just to avoid a warning because test_suite-tests has an implicit dependency on the lib crate of test_suite
