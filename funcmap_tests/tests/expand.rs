a451def58c1e44edbbec4f4f7b3cb39e4e47a28d
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-06-21 21:47:14 +0200

    chore: rustfmt no longer needed for macrotest

--- funcmap_tests/tests/expand.rs
+++ funcmap_tests/tests/expand.rs
@@ -1,10 +1,8 @@
 // Run expand tests only if preconditions for macrotest are satisfied:
 // - a nightly toolchain is used
 // - cargo-expand is installed
-// - rustfmt is installed
 #[rustversion::attr(not(nightly), ignore)]
 #[cfg_attr(not(has_cargo_expand), ignore)]
-#[cfg_attr(not(has_rustfmt), ignore)]
 #[allow(unused_attributes)] // don't warn on multiple #[ignore]
 #[test]
 fn expand() {
