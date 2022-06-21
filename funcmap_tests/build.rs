140003607e8162813579b545250c256431f1b6e2
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-06-21 21:43:47 +0200

    chore: patch version of prettyplease, re-enable expand tests with minimal versions

--- funcmap_tests/build.rs
+++ funcmap_tests/build.rs
@@ -22,10 +22,6 @@ fn has_rustfmt() -> bool {
     toolchain_find::find_installed_component("rustfmt").is_some()
 }
 
-fn is_minimal_versions() -> bool {
-    option_env!("FUNCMAP_MINIMAL_VERSIONS").is_some()
-}
-
 fn main() {
     if has_cargo_expand() {
         println!("cargo:rustc-cfg=has_cargo_expand");
@@ -33,9 +29,5 @@ fn main() {
 
     if has_rustfmt() {
         println!("cargo:rustc-cfg=has_rustfmt");
-    }
-
-    if is_minimal_versions() {
-        println!("cargo:rustc-cfg=is_minimal_versions");
     }
 }
