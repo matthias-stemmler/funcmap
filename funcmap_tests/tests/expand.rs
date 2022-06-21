815540b5d19dc0f67e747b944467d7757909ca1b
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-06-21 15:52:50 +0200

    test: ignore expand tests when using -Zminimal-versions

--- funcmap_tests/tests/expand.rs
+++ funcmap_tests/tests/expand.rs
@@ -5,6 +5,7 @@
 #[rustversion::attr(not(nightly), ignore)]
 #[cfg_attr(not(has_cargo_expand), ignore)]
 #[cfg_attr(not(has_rustfmt), ignore)]
+#[cfg_attr(is_minimal_versions, ignore)] // skip if using minimal cargo dependency versions as formatting of snapshots changes
 #[allow(unused_attributes)] // don't warn on multiple #[ignore]
 #[test]
 fn expand() {
