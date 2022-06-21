31718a217f87ef2f45505d76984f1837812338e9
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-06-21 15:52:50 +0200

    test: ignore expand tests when using -Zminimal-versions

--- funcmap_tests/build.rs
+++ funcmap_tests/build.rs
@@ -22,6 +22,10 @@ fn has_rustfmt() -> bool {
     toolchain_find::find_installed_component("rustfmt").is_some()
 }
 
+fn is_minimal_versions() -> bool {
+    option_env!("FUNCMAP_MINIMAL_VERSIONS").is_some()
+}
+
 fn main() {
     if has_cargo_expand() {
         println!("cargo:rustc-cfg=has_cargo_expand");
@@ -29,5 +33,9 @@ fn main() {
 
     if has_rustfmt() {
         println!("cargo:rustc-cfg=has_rustfmt");
+    }
+
+    if is_minimal_versions() {
+        println!("cargo:rustc-cfg=is_minimal_versions");
     }
 }
