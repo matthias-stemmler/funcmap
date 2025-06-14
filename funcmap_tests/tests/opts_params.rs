5708b634df7821d7e995d851d2b0f19b63b441d2
Matthias Stemmler <matthias.stemmler@gmail.com>
2025-06-14 20:57:00 +0200

    chore: fix warning

--- funcmap_tests/tests/opts_params.rs
+++ funcmap_tests/tests/opts_params.rs
@@ -4,19 +4,17 @@ use funcmap::{FuncMap, TypeParam};
 
 #[test]
 fn generics_to_be_mapped_can_be_configured() {
-    fn noop() {}
-
     #[derive(FuncMap, Debug, PartialEq)]
     #[funcmap(params(S, U))]
     struct Test<S, T, U> {
         value1: S,
-        not_mappable: fn() -> T,
+        not_mappable: *const T,
         value2: U,
     }
 
     let src = Test {
         value1: T1,
-        not_mappable: noop,
+        not_mappable: std::ptr::null::<()>(),
         value2: T1,
     };
     let dst = src
@@ -27,7 +25,7 @@ fn generics_to_be_mapped_can_be_configured() {
         dst,
         Test {
             value1: T2,
-            not_mappable: noop,
+            not_mappable: std::ptr::null::<()>(),
             value2: T2,
         }
     );
